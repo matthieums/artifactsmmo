e9168f99ab4c8f49233ea339ec1aa10f85f6680c
Matthieu <martens.matthieu@hotmail.com>
2025-04-13 13:27:42 +0200

    REFACTOR: Moved the logic to handle cooldown to a static function
    -Cooldown is displayed as it progresses toward 0
    -Functions are clearer and more concise

--- 2025_Python/models/character.py
+++ 2025_Python/models/character.py
@@ -10,31 +10,48 @@ class Character():
         self.position = position
         self.name = name
 
+    @staticmethod
+    async def handle_cooldown(data):
+        cooldown = data["data"]["cooldown"]["total_seconds"]
+        remaining = cooldown
+
+        while remaining > 0:
+            print(f"Cooldown remaining: {remaining} seconds")
+            sleep_time = min(5, remaining)
+            await asyncio.sleep(sleep_time)
+            remaining -= sleep_time
+
+        print("Ready for next action!")
+        return
+
     async def move_to(self, location: str) -> int:
         url, headers, data = get_url(character=self.name, action="move", location=location)
         response = requests.post(url=url, headers=headers, data=data)
         json_data = response.json()
-        print(f"{self.name} is moving towards {location}...")
-        await asyncio.sleep(json_data["data"]["cooldown"]["total_seconds"])
+        json_data = response.json()
+        print(f"{self.name} has moved to {location}...")
+        await self.handle_cooldown(json_data)
         print(response.status_code)
         print('move succesful')
         return response.status_code
 
     @check_character_position
-    def fight(self, monster: str) -> int:
+    async def fight(self, monster: str) -> int:
             url, headers = get_url(character=self.name, action="fight", location=monster)
             response = requests.post(url=url, headers=headers)
-            print(f"{self.name} fought {monster}")
+            print(f"{self.name} has fought {monster}")
+            json_data = response.json()
+            await self.handle_cooldown(json_data)
             print(response.text)
             return response.status_code
 
     async def rest(self):
         url, headers = get_url(character=self.name, action="rest")
         response = requests.post(url=url, headers=headers)
+        print(f"{self.name} has rested")
         json_data = response.json()
+        await self.handle_cooldown(json_data)
         print(response.text)
-        print(f"{self.name} is resting...")
-        await asyncio.sleep(json_data["data"]["cooldown"]["total_seconds"])
         return response.status_code
 
     def __repr__(self):
