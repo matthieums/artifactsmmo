e1207b893f4f1a2734af837fd84ea8cec871d184
Matthieu <martens.matthieu@hotmail.com>
2025-04-27 13:06:21 +0200

    FEAT: Add dictionary of ingredients and where to find them

--- 2025_Python/data/__init__.py
+++ 2025_Python/data/__init__.py
@@ -1,3 +1,4 @@
 from .locations import locations
 from .character_constants import SLOT_KEYS, XP_KEYS, HP_KEYS, COMBAT_KEYS
 from .actions_constants import bank_actions
+from .loot import lootable_from
