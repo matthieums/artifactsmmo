776c628113ba868f1660afa1cadc1e22aac66ef6
Matthieu <martens.matthieu@hotmail.com>
2025-05-05 13:24:47 +0200

    CHORE: Update init files

--- 2025_Python/data/__init__.py
+++ 2025_Python/data/__init__.py
@@ -2,3 +2,5 @@ from .locations import locations, reverse_locations
 from .character_constants import SLOT_KEYS, XP_KEYS, HP_KEYS, COMBAT_KEYS
 from .actions_constants import bank_actions
 from .loot import lootable_from
+from .monsters import monsters
+from .resources import resources
