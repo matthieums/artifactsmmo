2d57a14a9661421e4d988bf8e940bb538c031b6d
Matthieu <martens.matthieu@hotmail.com>
2025-05-09 16:35:18 +0200

    STYLE: Unindent overindented function

--- 2025_Python/data/utils/fight_data.py
+++ 2025_Python/data/utils/fight_data.py
@@ -12,32 +12,32 @@ logger = logging.getLogger(__name__)
 
 
 async def handle_fight_data(character: Character, response) -> None:
-        logger.info("handling fight data...")
+    logger.info("handling fight data...")
 
-        data = response.json()
-        fight_data = data['data']['fight']
-        loot = {}
+    data = response.json()
+    fight_data = data['data']['fight']
+    loot = {}
 
-        logger.info(f"{character} won the fight")
+    logger.info(f"{character} won the fight")
 
-        if fight_data.get("gold"):
-            gold = fight_data["gold"]
-            character.update_gold(gold)
-            loot["gold"] = gold
+    if fight_data.get("gold"):
+        gold = fight_data["gold"]
+        character.update_gold(gold)
+        loot["gold"] = gold
 
-        if fight_data.get("drops"):
-            items = defaultdict(int)
-            for item in fight_data["drops"]:
-                code, qty = item.get("code"), item.get("quantity")
-                character.update_inventory(code, qty)
-                items[code] += qty
-            loot["items"] = items
+    if fight_data.get("drops"):
+        items = defaultdict(int)
+        for item in fight_data["drops"]:
+            code, qty = item.get("code"), item.get("quantity")
+            character.update_inventory(code, qty)
+            items[code] += qty
+        loot["items"] = items
 
-        if fight_data.get("xp"):
-            loot["xp"] = fight_data.get("xp")
+    if fight_data.get("xp"):
+        loot["xp"] = fight_data.get("xp")
 
-        character.update_hp(data["data"]["character"]["hp"])
+    character.update_hp(data["data"]["character"]["hp"])
 
-        format_loot_message(character.name, loot)
+    format_loot_message(character.name, loot)
 
-        return
+    return
