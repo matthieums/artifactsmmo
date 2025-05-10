e902dd893857d7554ebadc35dc36f0a78d5963f8
Matthieu <martens.matthieu@hotmail.com>
2025-05-10 14:25:16 +0200

    FEAT: Add logic to make a post request to create a gather task

--- 2025_Python/api/server.py
+++ 2025_Python/api/server.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import state
+from pydantic import BaseModel
+from typing import List, Any, Dict
+import logging
 
+
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
@@ -17,3 +22,26 @@ app.add_middleware(
 @app.get("/")
 async def get_characters():
     return state.characters
+
+
+class TaskRequest(BaseModel):
+    iterations: int
+    character_index: int
+    function: str
+    args: List[Any] = []
+    kwargs: Dict[str, Any] = {}
+
+
+@app.post("/task")
+async def task(request: TaskRequest):
+    iterations = request.iterations
+    character = state.characters[request.character_index]
+    function = request.function
+    args = request.args
+    kwargs = request.kwargs
+
+    state.task_manager.add_task(
+        iterations, character, function, *args, **kwargs
+    )
+
+    return {"message": "Task added succesfully"}
