99f22fd7bc7cb6469ac321989b45bc9f4fa21240
Matthieu <martens.matthieu@hotmail.com>
2025-05-10 16:38:41 +0200

    REFACTOR: Apply factory pattern to the file.
    -Make the file more modular and maintainable

--- 2025_Python/utils/requests_factory.py
+++ 2025_Python/utils/requests_factory.py
@@ -1,25 +1,59 @@
+from typing import Optional, Any, Dict, Callable
 import config
 import json
-from data import maps
-from typing import Optional
 import httpx
-from utils.feedback import format_action_message
 import logging
+
 from utils.map import find_closest
+from utils.feedback import format_action_message
 
 logger = logging.getLogger(__name__)
-
 POST = "POST"
 GET = "GET"
 BASE_URL = "https://api.artifactsmmo.com"
 
 
-async def make_post_request(
-        url: str,
-        headers: dict,
-        data: dict | None = None
-        ):
+async def send_request(
+    character: Optional[str] = None,
+    action: Optional[str] = None,
+    location: Optional[str] = None,
+    item: Optional[str] = None,
+    slot: Optional[str] = None,
+    quantity: Optional[int] = None
+) -> tuple:
 
+    if action not in dispatcher:
+        raise KeyError("Action does not exist. Impossible request.")
+
+    kwargs = {
+        "character": character,
+        "location": location,
+        "item": item,
+        "slot": slot,
+        "quantity": quantity
+    }
+
+    handler = dispatcher[action]
+    response = await handler(**kwargs)
+
+    # Special case for map_data
+    if isinstance(response, list):
+        if len(response) > 1:
+            return response
+
+    if response.status_code == 200:
+        logger.debug("Request successful")
+        format_action_message(character, action, location)
+    else:
+        logger.debug(f"Error occured during request for {handler}")
+    return response
+
+
+async def make_post_request(
+    url: str,
+    headers: dict,
+    data: dict | None = None
+):
     kwargs = {
         "url": url,
         "headers": headers,
@@ -57,144 +91,6 @@ async def make_get_request(url: str, headers: dict, params: dict = None):
             return response
 
 
-async def send_request(
-    character: Optional[str] = None, action: Optional[str] = None,
-    location: Optional[str] = None, item: Optional[str] = None,
-    slot: Optional[str] = None, quantity: Optional[int] = None
-) -> tuple:
-
-    enabled_logging = True
-
-    if action in ["move", "map_data"]:
-        x, y = find_closest(character, location)
-        if action == "move":
-            headers = build_headers(POST)
-            data = {
-                "x": x,
-                "y": y
-            }
-            json_data = json.dumps(data)
-            url = f"{BASE_URL}/my/{character}/action/move"
-            response = await make_post_request(
-                url=url, headers=headers, data=json_data
-            )
-
-        elif action == "map_data":
-            headers = build_headers(GET)
-            url = f"{BASE_URL}/maps/{x}/{y}"
-            response = await make_get_request(url=url, headers=headers)
-
-    elif action == "fight":
-        headers = build_headers(POST)
-        url = f"{BASE_URL}/my/{character}/action/fight"
-        response = await make_post_request(url=url, headers=headers)
-
-    elif action == "rest":
-        headers = build_headers(POST)
-        url = f"{BASE_URL}/my/{character}/action/rest"
-        response = await make_post_request(url=url, headers=headers)
-
-    elif action == "gather":
-        headers = build_headers(POST)
-        url = f"{BASE_URL}/my/{character}/action/gathering"
-        response = await make_post_request(url=url, headers=headers)
-
-    elif action == "craft":
-        if not item:
-            raise Exception("Missing argument: item")
-        headers = build_headers(POST)
-        json_data = json.dumps({
-            "code": item
-        })
-        url = f"{BASE_URL}/my/{character}/action/crafting"
-        response = await make_post_request(
-            url=url, headers=headers, data=json_data
-        )
-
-    elif action in ["equip", "unequip"]:
-        headers = build_headers(POST)
-        url = f"{BASE_URL}/my/{character}/action/{action}"
-        data = {
-            "code": item,
-            "slot": slot
-        }
-        json_data = json.dumps(data)
-        response = await make_post_request(
-            url=url, headers=headers, data=json_data
-        )
-
-    elif action == "item_info":
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/items/{item}"
-        response = await make_get_request(url=url, headers=headers)
-
-    elif action in ["withdraw", "deposit", "empty_inventory"]:
-        headers = build_headers(POST)
-        url = f"{BASE_URL}/my/{character}/action/bank/{action}"
-        data = {
-            "code": item,
-            "quantity": quantity
-        }
-        json_data = json.dumps(data)
-        response = await make_post_request(
-            url=url, headers=headers, data=json_data
-        )
-
-    elif action == "char_data":
-        enabled_logging = False
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/my/characters"
-        response = await make_get_request(url=url, headers=headers)
-
-    elif action == "bank_details":
-        enabled_logging = False
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/my/bank"
-        response = await make_get_request(url=url, headers=headers)
-
-    elif action == "bank_items":
-        enabled_logging = False
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/my/bank/items"
-        response = await make_get_request(url=url, headers=headers)
-
-    elif action == "get_all_monsters":
-        enabled_logging = False
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/monsters"
-        params = {"size": 100}
-        response = await make_get_request(
-            url=url, headers=headers, params=params
-        )
-
-    elif action == "get_all_resources":
-        enabled_logging = False
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/resources"
-        params = {"size": 100}
-        response = await make_get_request(
-            url=url, headers=headers, params=params
-        )
-
-    elif action == "get_all_maps":
-        enabled_logging = False
-        headers = build_headers(GET)
-        url = f"{BASE_URL}/maps"
-        responses = []
-        for i in range(1, 5):
-            params = {"page": i, "size": 100}
-            response = await make_get_request(
-                url=url, headers=headers, params=params
-            )
-            responses.append(response)
-        response = responses
-
-    if enabled_logging and response.is_success:
-        format_action_message(character, action, location)
-
-    return response
-
-
 def build_headers(request: str) -> dict:
     if request == POST:
         headers = {
@@ -204,10 +100,165 @@ def build_headers(request: str) -> dict:
         }
         return headers
 
-    elif request == 'GET':
+    elif request == GET:
         headers = {
             "Accept": "application/json",
             "Content-Type": "application/json",
             "Authorization": f"Bearer {config.API_KEY}"
         }
         return headers
+
+
+async def move_action(character, location, *args, **kwargs):
+    x, y = find_closest(character, location)
+    headers = build_headers(POST)
+    data = {
+        "x": x,
+        "y": y
+    }
+    json_data = json.dumps(data)
+    url = f"{BASE_URL}/my/{character}/action/move"
+    return await make_post_request(
+        url=url, headers=headers, data=json_data
+    )
+
+
+async def map_data(character, location, *args, **kwargs):
+    x, y = find_closest(character, location)
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/maps/{x}/{y}"
+    return await make_get_request(url=url, headers=headers)
+
+
+async def fight_action(character, *args, **kwargs):
+    headers = build_headers(POST)
+    url = f"{BASE_URL}/my/{character}/action/fight"
+    return await make_post_request(url=url, headers=headers)
+
+
+async def rest_action(character, *args, **kwargs):
+    headers = build_headers(POST)
+    url = f"{BASE_URL}/my/{character}/action/rest"
+    return await make_post_request(url=url, headers=headers)
+
+
+async def gather_action(character, *args, **kwargs):
+    headers = build_headers(POST)
+    url = f"{BASE_URL}/my/{character}/action/gathering"
+    return await make_post_request(url=url, headers=headers)
+
+
+async def craft_action(character, item, *args, **kwargs):
+    if not item:
+        raise Exception("Missing argument: item")
+    headers = build_headers(POST)
+    json_data = json.dumps({
+        "code": item
+    })
+    url = f"{BASE_URL}/my/{character}/action/crafting"
+    return await make_post_request(
+        url=url, headers=headers, data=json_data
+    )
+
+
+async def equip_action(character, action, item, slot, *args, **kwargs):
+    headers = build_headers(POST)
+    url = f"{BASE_URL}/my/{character}/action/{action}"
+    data = {
+        "code": item,
+        "slot": slot
+    }
+    json_data = json.dumps(data, *args, **kwargs)
+    return await make_post_request(
+        url=url, headers=headers, data=json_data
+    )
+
+
+async def item_info(item, *args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/items/{item}"
+    return await make_get_request(url=url, headers=headers)
+
+
+async def bank_action(character, action, item, quantity, *args, **kwargs):
+    headers = build_headers(POST)
+    url = f"{BASE_URL}/my/{character}/action/bank/{action}"
+    data = {
+        "code": item,
+        "quantity": quantity
+    }
+    json_data = json.dumps(data)
+    return await make_post_request(
+        url=url, headers=headers, data=json_data
+    )
+
+
+async def get_characters(*args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/my/characters"
+    return await make_get_request(url=url, headers=headers)
+
+
+async def bank_details(*args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/my/bank"
+    return await make_get_request(url=url, headers=headers)
+
+
+async def bank_items(*args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/my/bank/items"
+    return await make_get_request(url=url, headers=headers)
+
+
+async def get_all_monsters(*args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/monsters"
+    params = {"size": 100}
+    return await make_get_request(
+        url=url, headers=headers, params=params
+    )
+
+
+async def get_all_resources(*args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/resources"
+    params = {"size": 100}
+    return await make_get_request(
+        url=url, headers=headers, params=params
+    )
+
+
+async def get_all_maps(*args, **kwargs):
+    headers = build_headers(GET)
+    url = f"{BASE_URL}/maps"
+    responses = []
+    for i in range(1, 5):
+        params = {"page": i, "size": 100}
+        response = await make_get_request(
+            url=url, headers=headers, params=params
+        )
+        responses.append(response)
+    return responses
+
+
+dispatcher: Dict[str, Callable[..., Any]] = {
+    "move": move_action,
+    "map_data": map_data,
+    "fight": fight_action,
+    "rest": rest_action,
+    "gather": gather_action,
+    "craft": craft_action,
+    "equip": equip_action,
+    "unequip": equip_action,
+    "item_info": item_info,
+    "char_data": get_characters,
+    "withdraw": bank_action,
+    "empty": bank_action,
+    "deposit": bank_action,
+    "bank_details": bank_details,
+    "bank_items": bank_items,
+    "get_all_monsters": get_all_monsters,
+    "get_all_resources": get_all_resources,
+    "get_all_maps": get_all_maps,
+}
